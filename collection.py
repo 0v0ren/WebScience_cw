8ebce3c53d87a57e07e1622b387ab3cdfbb9f15c
0v0ren <yurenyan11@gmail.com>
2020-03-23 10:58:08 +0000

    Update collection.py

--- collection.py
+++ collection.py
@@ -8,11 +8,19 @@ i = 0
 MONGO_HOST = 'mongodb://localhost:27017/twitter_database' 
 FILE_NAME = "tweets_data5.json"
 
-consumer_key="dQtJlDTYY1qrDWD3gThw7WTd3"
-consumer_secret="9wo2doXpcs9bGDy7poUrMsaldSgyJ2dUK6SYeLJxbyTH75uKKP"
+consumer_key="******"
+consumer_secret="******"
 
-access_token="1230476879704010752-zLX1QEeGN6HZ2eJpxkUjF2GILtbNQX"
-access_token_secret="mlhoO4ZDi1BNN57VL6tyhdsc2oefR0Km4LGAiVkONi8xj"
+access_token="*******"
+access_token_secret="******"
+keywords =" happy"
+ #["#happy","#joy","#love"]
+ #["#pleasant","#trust","#like","#admiration"]
+ #["#Surprise","#sad","#frustration","#sadness","#amazement","#distracting"]
+ #["#fear","#disgust","#depression","#shy","#fearful","#terrifies"]
+ #["#angry","#anger","#roar","#hormonal","#veryangry","#temper"]
+ #["#excitement","#anticipation","#expecting","#interested","#interesting"]
+
 class StdOutListener(StreamListener):
      
         
@@ -28,9 +36,12 @@ class StdOutListener(StreamListener):
             client = MongoClient(MONGO_HOST)  # connect mongodb
             db = client.twitter_database  # create db
             data_json = json.loads(data)  # Decode the JSON from Twitter
-            #db.twitterdb_Excitement.insert(data_json)  # insert the data into the mongodb into a collection
-            db.twitterdb_Happy.insert(data_json)
-            #db.twitterdb_Pleasant1.insert(data_json)
+            db.twitterdb.insert(data_json) 
+            # insert the data into the mongodb into a collection
+            
+            db.twitterdb_Excitement.insert(data_json) 
+            #db.twitterdb_Happy.insert(data_jso)n)
+            #db.twitterdb_Pleasant1.insert(data_json
             #db.twitterdb_ .insert(data_json)
             #db.twitterdb_Fear.insert(data_json)
             #db.twitterdb_Angry.insert(data_json)
@@ -54,6 +65,31 @@ class StdOutListener(StreamListener):
         print("ERROR")
         print(status)          
 	
+if __name__ == '__main__':
+	try:
+		#Create a file to store output. "a" means append (add on to previous file)
+		fhOut = open("output.json","a")
+
+		#Create the listener
+		l = StdOutListener()
+		auth = OAuthHandler(consumer_key, consumer_secret)
+		auth.set_access_token(access_token, access_token_secret)
+		#Connect to the Twitter stream
+		stream = Stream(auth, l)	
+		#Terms to track
+		stream.filter(track=keywords)
+        stream.filter(language=['en'])
+		
+		
+	except KeyboardInterrupt:
+		#User pressed ctrl+c -- get ready to exit the program
+		pass
+
+	#Close the 
+	fhOut.close()
+    
+    
+  
 
 
 
